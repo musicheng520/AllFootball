a0f78936c155233cd84cb17d923eddc02106e288
Sicheng Mu <musicheng520@gmail.com>
2026-02-11 20:41:38 +0000

    Week0 Day1: initial Spring Boot setup with secure config structure

---
+++ src/main/java/com/msc/allfootball/allfootball/AllFootballApplication.java
@@ -0,0 +1,13 @@
+package com.msc.allfootball.allfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllFootballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllFootballApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msc/allfootball/allfootball/AllFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.allfootball.allfootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllFootballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
